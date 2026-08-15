8f46dc0d667a60cb16508bbed2941257545b5650
PRATYA WATTANASRIROJ <pratchaya.jin@gmail.com>
2026-08-16 01:21:55 +0700

    jwks

--- app.py
+++ app.py
@@ -43,6 +43,7 @@ google = oauth.register(
     access_token_url='https://accounts.google.com/o/oauth2/token',
     authorize_url='https://accounts.google.com/o/oauth2/auth',
     api_base_url='https://www.googleapis.com/oauth2/v1/',
+    jwks_uri='https://www.googleapis.com/oauth2/v3/certs',
     client_kwargs={'scope': 'openid email profile'},
 )
 
